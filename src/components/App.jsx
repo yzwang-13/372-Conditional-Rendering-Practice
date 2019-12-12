39b5b32d57716baf16f6b19f041319ddd0001682
yzwang-13 <53463432+yzwang-13@users.noreply.github.com>
2019-12-12 21:35:12 +1100

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Form from "./Form";
+//Challenge: Without moving the userIsRegistered variable,
+//1. Show Login as the button text if userIsRegistered is true.
+//Show Register as the button text if userIsRegistered is false.
+//2. Only show the Confirm Password input if userIsRegistered is false.
+//Don't show it if userIsRegistered is true.
 
-var userIsRegistered = false;
+var userIsRegistered = true;
 
 function App() {
   return (
     <div className="container">
-      <Form />
+      <Form registered={userIsRegistered} />
     </div>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import Input from "./Input";
 
-function Form() {
+function Form(props) {
+  console.log(props);
   return (
     <form className="form">
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <input type="password" placeholder="Confirm Password" />
-      <button type="submit">Register</button>
+      <Input type="text" placeholder="Username" />
+      <Input type="password" placeholder="Password" />
+      {!props.registered && (
+        <Input type="password" placeholder="Confirm Password" />
+      )}
+      <button type="submit">{props.registered ? "Login" : "Register"}</button>
     </form>
   );
 }
